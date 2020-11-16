2355cd29d5ba4c624d0d50fdfb57944364e986fa
Maja Gedda <maja.gedda@zocom.se>
2020-11-16 13:53:41 +0100

    them insults

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const url = 'https://shakespeare-insults-generator.herokuapp.com/getInsult';
 const btn = document.getElementById('btn');
 const insultElement = document.getElementById('insult');
+const playElement = document.getElementById('play');
+
+let showInsult = (insult, play) => {
+    insultElement.innerHTML = insult;
+    playElement.innerHTML = play;
+}
+
+let getInsult = () => {
+    fetch(url)
+    .then(response => response.json())
+    .then(response => showInsult(response.insult, response.play))
+}
+
+btn.addEventListener('click', getInsult);
